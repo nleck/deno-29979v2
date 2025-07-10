39d4b97db692bac13cb882194142c547b5ac5290
Nigel Leck <nigel@stsoftware.com.au>
2025-07-10 15:38:46 +1000

    Update run.sh to set Deno version to v2.3.6 and reduce timeout from 45 to 15 seconds.

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@
 set -e
 BASE_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 cd "${BASE_DIR}"
+version="v2.3.6" # Works 
+# version="v2.4.1" # Fails 
 
+deno upgrade ${version}
 echo -n "Starting... "
 date
 
@@ -10,7 +13,7 @@ deno run \
   --v8-flags=--max-old-space-size=16384 \
   --allow-read --allow-write --allow-net \
   src/Learn.ts \
-  --timeout=45 \
+  --timeout=15 \
   --dir=./creatures \
   --verbose=true 
 
